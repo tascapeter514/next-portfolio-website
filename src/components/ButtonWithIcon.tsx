9d9b049c7bc3f85d6297ef372bbd54eefbea1e1d
petertasca <tascapeter@gmail.com>
2025-05-21 15:54:45 -0400

    feat: button spin animation

--- src/components/ButtonWithIcon.tsx
+++ src/components/ButtonWithIcon.tsx
@@ -4,8 +4,8 @@ import { Button } from '@/components/ui/button'
 const ButtonWithIcon = () => {
 
     return(
-        <Button className='w-full max-w-[250px] flex justify-center text-base p-[2em]'>
-            See What I Can Do For You<ChevronRight className='size-5'/> 
+        <Button className='relative w-full max-w-[250px] flex justify-center text-base p-[2em] overflow:hidden shadow-2xl transition-all duration-300 before:absolute before:inset-0 before:border-0 before:border-white before:duration-100 before:ease-linear hover:bg-white hover:text-black hover:shadow-black-600 hover:before:border-[15px] hover:before:rounded-xl'>
+            See What I Can Do For You <ChevronRight className='size-5 animate-spin'/> 
         </Button> 
     )
 
