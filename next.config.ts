e9626fc13cba496e8f2fd887ea69404fd2d3a9e1
petertasca <tascapeter@gmail.com>
2025-05-29 09:25:27 -0400

    fix: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY
+  }
 };
 
 export default nextConfig;
